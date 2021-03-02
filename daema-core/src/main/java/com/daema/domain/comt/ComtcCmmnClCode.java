c7c63da31a26252cdea2185c81d3450118e88bbf
rhko <rhfogud@naver.com>
2021-03-02 09:56:48 +0900

    자동 생성 entity 조정. 샘플

--- daema-core/src/main/java/com/daema/domain/comt/ComtcCmmnClCode.java
+++ daema-core/src/main/java/com/daema/domain/comt/ComtcCmmnClCode.java
@@ -1,46 +1,47 @@
 package com.daema.domain.comt;
 
-import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-import javax.persistence.*;
-import java.sql.Date;
-import java.sql.Timestamp;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
-@EqualsAndHashCode(of="cl_code")
+@EqualsAndHashCode(of="clCode")
 @ToString
 @Entity
 @Table(name = "comtc_cmmn_cl_code")
 public class ComtcCmmnClCode {
 
     @Id
-    @Column(name = "cl_code")
+    @Column(name = "cl_code", length = 3, nullable = false)
     private String clCode;
 
-    @Column(name = "cl_code_nm")
+    @Column(name = "cl_code_nm", length = 60)
     private String clCodeNm;
 
-    @Column(name = "cl_code_dc")
+    @Column(name = "cl_code_dc", length = 200)
     private String clCodeDc;
 
-    @Column(name = "use_at")
+    @Column(name = "use_at", length = 1)
     private String useAt;
 
     @Column(name = "frst_regist_pnttm")
-    private java.sql.Timestamp frstRegistPnttm;
+    private LocalDateTime frstRegistPnttm;
 
-    @Column(name = "frst_register_id")
+    @Column(name = "frst_register_id", length = 20)
     private String frstRegisterId;
 
     @Column(name = "last_updt_pnttm")
-    private java.sql.Timestamp lastUpdtPnttm;
+    private LocalDateTime lastUpdtPnttm;
 
-    @Column(name = "last_updusr_id")
+    @Column(name = "last_updusr_id", length = 20)
     private String lastUpdusrId;
 
 
