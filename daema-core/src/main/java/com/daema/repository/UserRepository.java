e1f882ca89f03b5cefc5a5faa9276ab2cb7b648f
Luther Lee <fashionday2@gmail.com>
2021-03-08 14:46:43 +0900

    사용자 엔티티 추가

---
+++ daema-core/src/main/java/com/daema/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.daema.repository;
+
+import com.daema.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUserId(String userId);
+    boolean existsByUserIdANdUserPw(String userId, String userPw);
+}
