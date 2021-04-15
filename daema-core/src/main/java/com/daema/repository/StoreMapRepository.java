4cbb55a69ab5f431825b74a838822eb0d5070f87
rhko <rhfogud@naver.com>
2021-04-15 11:04:39 +0900

    CustomStoreMapRepository.java 제거

--- daema-core/src/main/java/com/daema/repository/StoreMapRepository.java
+++ daema-core/src/main/java/com/daema/repository/StoreMapRepository.java
@@ -4,6 +4,6 @@ import com.daema.domain.StoreMap;
 import com.daema.domain.pk.StoreMapPK;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StoreMapRepository extends JpaRepository<StoreMap, StoreMapPK>, CustomStoreMapRepository {
+public interface StoreMapRepository extends JpaRepository<StoreMap, StoreMapPK> {
     StoreMap findByStoreIdAndParentStoreId(long storeId, long parentStoreId);
 }
