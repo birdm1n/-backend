4b7d05bda5757e34ca00760def2b8ca4dd0cd2c6
rhko <rhfogud@naver.com>
2021-05-14 16:08:34 +0900

    - entity 변경

---
+++ daema-core/src/main/java/com/daema/wms/domain/DeviceJudge.java
@@ -0,0 +1,35 @@
+package com.daema.wms.domain;
+
+import com.daema.base.domain.common.BaseEntity;
+import com.daema.wms.domain.enums.WmsEnum;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Builder
+@Getter
+@Setter
+@EqualsAndHashCode(of = "dvcId")
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "device_judge")
+public class DeviceJudge extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "dvc_judge_id")
+    private Long dvcJudgeId;
+
+    @Column(name = "judge_memo")
+    private String judgeMemo;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "judge_status")
+    private WmsEnum.JudgementStatus judgeStatus;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "dvc_id")
+    private Device device;
+}
