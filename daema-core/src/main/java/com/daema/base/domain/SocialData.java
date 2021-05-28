ccc348b08950df0a7cb083a4ba832e2bfa96b0c6
rhko <rhfogud@naver.com>
2021-05-28 13:53:10 +0900

    미사용 컬럼 삭제

--- daema-core/src/main/java/com/daema/base/domain/SocialData.java
+++ daema-core/src/main/java/com/daema/base/domain/SocialData.java
@@ -21,9 +21,6 @@ public class SocialData {
     private String email;
     private String type;
 
-    @Column(columnDefinition = "varchar(255) comment '이름'")
-    private String testComment;
-
     @Column( columnDefinition = "BIGINT UNSIGNED comment '이름'")
     private Long unsignedTest;
 
